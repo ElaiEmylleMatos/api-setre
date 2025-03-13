02262c63c5245e0ac5febb61ed2038dbcdfb5d54
tassiosantos <tassiosantos@gmail.com>
2025-03-12 22:02:26 -0300

    Criação do projeto.

---
+++ src/main/java/com/restic36/api_setre/ApiSetreApplication.java
@@ -0,0 +1,13 @@
+package com.restic36.api_setre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSetreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSetreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restic36/api_setre/ApiSetreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restic36.api_setre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSetreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
